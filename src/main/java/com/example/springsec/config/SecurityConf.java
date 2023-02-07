16b4a9e568e28f322c95407ec6f01fe2b8cab87a
Denis Znobishin <znobishindi@yandex.ru>
2023-02-07 21:40:01 +0300

    ДЗ "Method security" выполнено

--- src/main/java/com/example/springsec/config/SecurityConf.java
+++ src/main/java/com/example/springsec/config/SecurityConf.java
@@ -1,19 +1,42 @@
 package com.example.springsec.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConf extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Denis").password("{noop}qwerty1").authorities("read","write")
+                .withUser("Denis").password(encoder().encode("qwerty1"))
+                .authorities("read", "write")
                 .and()
-                .withUser("Elena").password("{noop}qwerty2").authorities("read")
+                .withUser("Elena").password(encoder().encode("qwerty2"))
+                .authorities("read")
                 .and()
-                .withUser("Sergey").password("{noop}qwerty3").authorities("write");
+                .withUser("Sergey").password(encoder().encode("qwerty3"))
+                .authorities("write")
+                .and()
+                .withUser("Vasily").password(encoder().encode("qwerty4"))
+                .roles("READ", "WRITE")
+                .and()
+                .withUser("Maxim").password(encoder().encode("qwerty5"))
+                .roles("READ")
+                .and()
+                .withUser("Petr").password(encoder().encode("qwerty5"))
+                .roles("Write");
     }
 
     @Override
--- src/main/java/com/example/springsec/controller/RestController.java
+++ src/main/java/com/example/springsec/controller/RestController.java
@@ -1,7 +1,12 @@
 package com.example.springsec.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.security.RolesAllowed;
 
 @org.springframework.web.bind.annotation.RestController
 @SpringBootApplication
@@ -12,19 +17,33 @@ public class RestController {
         return "Привет неавторизованный пользователь";
     }
 
+    @PreAuthorize("hasAnyRole('READ','WRITE')")
     @GetMapping("/hello")
-        public String hello(){
-          return "привет авторизованный пользователь";
-        }
+    public String hello() {
+        return "привет авторизованный пользователь";
+    }
+    @PostAuthorize("hasAnyRole('READ','WRITE')")
+    @GetMapping("/hello-peace")
+    public String helloPeace() {
+        return "привет авторизованный пользователь и мира тебе!";
+    }
 
+    @Secured({"ROLE_READ"})
     @GetMapping("/read")
-    public String read(){
+    public String read() {
         return "Ну давай что-нибудь прочитаем";
     }
 
+    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/write")
-    public String write(){
+    public String write() {
         return "Ну давай что-нибудь напишем";
     }
+    @PreAuthorize("#userName == authentication.principal.username")
+    @GetMapping("/good-bye")
+    public String goodBye(String userName) {
+        return "Ну давай, " + userName + "! Еще увидимся!";
+    }
+
 
 }
